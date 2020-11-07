621c62a65e6cc4293837fe2360595a858c3db081
harcop <harcopdrew@gmail.com>
2020-11-07 22:41:33 +0100

    feat(): add graphql query

--- server/schemas/schema.js
+++ server/schemas/schema.js
@@ -1,12 +1,34 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
-
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+    { name: 'The final Empire', genre: 'Fantasy', id: '2'},
+    { name: 'The long Earth', genre: 'Sci-Fi', id: '3'},
+]
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString } 
     })
 })
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLID }},
+            resolve(parent, args) {
+                console.log(typeof args.id)
+                return books.find(book => book.id === args.id);
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
